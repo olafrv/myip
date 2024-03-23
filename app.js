29a292f226b1a5285a1b92ee8dd4f463b154e9d0
Olaf Reitmaier <olafrv@gmail.com>
2024-03-24 00:45:44 +0100

    Rework

--- app.js
+++ app.js
@@ -8,38 +8,34 @@ const app = express();
 app.use((req, res, next) => {
     const clientIP = req.ip || req.socket.remoteAddress;
     const token = req.query.token || '';
-    if(token !== env.MYIP_TOKEN){
+    if(token != env.MYIP_TOKEN){
+        console.log(clientIP + ': Unauthorized!');
         res.status(403).json({
             error: 'Unauthorized'
         });
-        console.log(clientIP + ': Unauthorized!');
         return;
     }
     req.clientIP = clientIP;
-    maxmind.open('./dbs/GeoLite2-ASN.mmdb').then(reader => {
-        try {
-            const response = reader.asn(clientIP);
-            req.asn = response.Asn.autonomousSystemOrganization;
-        }catch(error){
-            console.log(error.message);
-            req.asn = undefined;
-        }
-    });
     next();
 });
 
 app.get('/', (req, res) => {
-    if (req.asn === undefined){
-        res.json({
-            ip: req.clientIP,
-            asn: 'unknown'
-        });
-    }else{
-        res.json({
-            ip: req.clientIP,
-            asn: req.asn
-        });
-    }
+    maxmind.open('./dbs/GeoLite2-ASN.mmdb').then(reader => {
+        try {
+            const query = reader.asn(req.clientIP);
+            req.clientASN = query.autonomousSystemOrganization;
+            console.log(req.clientIP + ': ' + req.clientASN);
+            res.json({
+                    ip: req.clientIP,
+                    asn: req.clientASN
+            });
+        }catch(error){
+            console.log(req.clientIP + ': ' + error.message);
+            res.status(404).json({
+                    error: 'Not found'
+            });
+        }
+    });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
